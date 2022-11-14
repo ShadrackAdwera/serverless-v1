b88ee9f547647752a392d15ca7c58b90b3b804a0
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-14 13:19:00 +0300

    query products using the scan command

--- src/products/controllers/product-controllers.ts
+++ src/products/controllers/product-controllers.ts
@@ -1,5 +1,10 @@
 import { ddbClient } from '../libs/ddbClient';
-import { GetItemCommand, GetItemCommandInput } from '@aws-sdk/client-dynamodb';
+import {
+  GetItemCommand,
+  GetItemCommandInput,
+  ScanCommand,
+  ScanCommandInput,
+} from '@aws-sdk/client-dynamodb';
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 
 const getProductById = async (productId: string) => {
@@ -16,4 +21,19 @@ const getProductById = async (productId: string) => {
   }
 };
 
-export { getProductById };
+const getProducts = async () => {
+  const params: ScanCommandInput = {
+    TableName: process.env.DYNAMODB_TABLE_NAME,
+  };
+  try {
+    const { Items } = await ddbClient.send(new ScanCommand(params));
+    return Items && Items?.length > 0
+      ? Items.map((Item) => unmarshall(Item))
+      : [];
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export { getProductById, getProducts };
