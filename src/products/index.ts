1d271a3671e8a894ab5d31c53eceb9c26c41f42d
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-14 17:39:58 +0300

    refactor switch statement

--- src/products/index.ts
+++ src/products/index.ts
@@ -13,65 +13,78 @@ exports.handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  let apiResponse: APIGatewayProxyResult = {
-    statusCode: 404,
-    body: JSON.stringify({
-      message: 'Invalid method / route provided.',
-    }),
-  };
-  switch (event.httpMethod) {
-    case 'GET':
-      let productBody: Record<string, any> | Record<string, any>[];
-      if (event.queryStringParameters !== null) {
-        productBody = await getProductsByCategory(event);
-      } else if (
-        event.pathParameters !== null &&
-        event.pathParameters.id !== undefined
-      ) {
-        productBody = await getProductById(event.pathParameters.id);
-      } else {
-        productBody = await getProducts();
-      }
-      apiResponse = {
-        statusCode: 200,
-        body: JSON.stringify(productBody),
-      };
-      break;
-    case 'POST':
-      const result = await createProduct(event);
-      apiResponse = {
-        statusCode: 201,
-        body: JSON.stringify({
-          message: 'Product Created',
-          product: result.Attributes,
-        }),
-      };
-      break;
-    case 'PATCH':
-      await updateProduct(event);
-      apiResponse = {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: 'Product Updated',
-        }),
-      };
-      break;
-    case 'DELETE':
-      if (
-        event.pathParameters !== null &&
-        event.pathParameters.id !== undefined
-      ) {
-        await deleteProduct(event.pathParameters.id);
+  console.log(`Request: ${JSON.stringify(event, undefined, 2)}`);
+  try {
+    let apiResponse: APIGatewayProxyResult = {
+      statusCode: 404,
+      body: JSON.stringify({
+        message: 'Invalid method / route provided.',
+      }),
+    };
+    switch (event.httpMethod) {
+      case 'GET':
+        let productBody: Record<string, any> | Record<string, any>[];
+        if (
+          event.queryStringParameters &&
+          event.queryStringParameters.category
+        ) {
+          productBody = await getProductsByCategory(event);
+        } else if (event.pathParameters && event.pathParameters.id) {
+          productBody = await getProductById(event.pathParameters.id);
+        } else {
+          productBody = await getProducts();
+        }
+        apiResponse = {
+          statusCode: 200,
+          body: JSON.stringify(productBody),
+        };
+        break;
+      case 'POST':
+        const result = await createProduct(event);
+        apiResponse = {
+          statusCode: 201,
+          body: JSON.stringify({
+            message: 'Product Created',
+            product: result.Attributes,
+          }),
+        };
+        break;
+      case 'PATCH':
+        await updateProduct(event);
         apiResponse = {
           statusCode: 200,
           body: JSON.stringify({
-            message: 'Product Deleted',
+            message: 'Product Updated',
           }),
         };
-      }
-      break;
-    default:
-      throw new Error('Invalid method / route');
+        break;
+      case 'DELETE':
+        if (
+          event.pathParameters !== null &&
+          event.pathParameters.id !== undefined
+        ) {
+          await deleteProduct(event.pathParameters.id);
+          apiResponse = {
+            statusCode: 200,
+            body: JSON.stringify({
+              message: 'Product Deleted',
+            }),
+          };
+        }
+        break;
+      default:
+        throw new Error('Invalid method / route');
+    }
+    return apiResponse;
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Internal Server Error',
+        errorMessage:
+          error instanceof Error ? error.message : 'Internal Server Error',
+        errorStack: error instanceof Error ? error.stack : '',
+      }),
+    };
   }
-  return apiResponse;
 };
