35784ac51700dfec49b44b3738c6c5eb6ae6d57a
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-15 14:56:20 +0300

    separate infrastructure based on resource type

--- lib/serverless-v1-stack.ts
+++ lib/serverless-v1-stack.ts
@@ -1,59 +1,20 @@
-import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import {
-  NodejsFunction,
-  NodejsFunctionProps,
-} from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 
 import { Construct } from 'constructs';
-import * as path from 'path';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { ServerlessV1Database } from './database/infrastructure';
+import { ServerlessV1Microservices } from './api/microservices';
+import { ServerlessV1ApiGateway } from './api/api-gateway';
 
 export class ServerlessV1Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const productTable = new Table(this, 'products', {
-      partitionKey: { name: 'id', type: AttributeType['STRING'] },
-      tableName: 'products',
-      billingMode: BillingMode['PAY_PER_REQUEST'],
-      removalPolicy: RemovalPolicy['DESTROY'],
+    const database = new ServerlessV1Database(this, 'Database');
+    const microservice = new ServerlessV1Microservices(this, 'Microservice', {
+      productsTable: database.productsTable,
     });
-
-    const nodeJsFnProps: NodejsFunctionProps = {
-      bundling: {
-        externalModules: ['aws-sdk'],
-        minify: true,
-      },
-      environment: {
-        PRIMARY_KEY: 'id',
-        DYNAMODB_TABLE_NAME: productTable.tableName,
-      },
-      runtime: Runtime['NODEJS_16_X'],
-    };
-
-    const productFn = new NodejsFunction(this, 'productsLambdaFn', {
-      entry: path.join(__dirname, '/../src/products/index.ts'),
-      ...nodeJsFnProps,
+    const apiGateway = new ServerlessV1ApiGateway(this, 'ApiGateway', {
+      productFn: microservice.productFn,
     });
-
-    productTable.grantReadWriteData(productFn);
-
-    const apiGateway = new LambdaRestApi(this, 'productsApi', {
-      restApiName: 'Products Service API',
-      handler: productFn,
-      proxy: false,
-    });
-    const products = apiGateway.root.addResource('products');
-    products.addMethod('GET'); // GET /products
-    products.addMethod('POST'); // POST /products
-
-    // single product
-    const product = products.addResource('{id}'); // /products/:id
-    product.addMethod('GET'); // GET product by id
-    product.addMethod('PATCH'); // PATCH update product
-    product.addMethod('DELETE'); // DELETE delete product
   }
 }
