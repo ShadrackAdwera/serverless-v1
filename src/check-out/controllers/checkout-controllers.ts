b1acb56703aa11cacacc580a5e9062c8f870c1de
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-17 15:56:45 +0300

    create GET checkout scan

--- src/check-out/controllers/checkout-controllers.ts
+++ src/check-out/controllers/checkout-controllers.ts
@@ -1,17 +1,48 @@
-import { GetItemCommand, GetItemCommandInput } from '@aws-sdk/client-dynamodb';
+import {
+  ScanCommand,
+  ScanCommandInput,
+  GetItemCommand,
+  GetItemCommandInput,
+} from '@aws-sdk/client-dynamodb';
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 import { ddbClient } from '../libs/ddbClient';
 
-const getCheckOut = async (username: string) => {
+const getCheckOutByUsername = async (username: string) => {
   const params: GetItemCommandInput = {
     TableName: process.env.DYNAMODB_TABLE_NAME,
     Key: marshall({ username }),
   };
   try {
     const { Item } = await ddbClient.send(new GetItemCommand(params));
-    return Item ? unmarshall(Item) : [];
+    return Item
+      ? {
+          checkout: unmarshall(Item),
+        }
+      : [];
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
+
+const getCheckOut = async () => {
+  const params: ScanCommandInput = {
+    TableName: process.env.DYNAMODB_TABLE_NAME,
+  };
+  try {
+    const { Items } = await ddbClient.send(new ScanCommand(params));
+    return Items && Items.length > 0
+      ? {
+          count: Items.length,
+          items: Items.map((item) => unmarshall(item)),
+        }
+      : {
+          message: 'No items found for this checkout',
+        };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export { getCheckOutByUsername, getCheckOut };
