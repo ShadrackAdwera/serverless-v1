bae24a4fbbc4baa54deca67b8e44a0d9231104c0
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-21 11:35:31 +0300

    create orders microservice construct

--- lib/api/microservices.ts
+++ lib/api/microservices.ts
@@ -11,11 +11,13 @@ import * as path from 'path';
 interface IServerlessV1MicroserviceProps {
   productsTable: ITable;
   checkOutTable: ITable;
+  ordersTable: ITable;
 }
 
 export class ServerlessV1Microservices extends Construct {
   public readonly productFn: NodejsFunction;
   public readonly checkoutFn: NodejsFunction;
+  public readonly ordersFn: NodejsFunction;
   constructor(
     scope: Construct,
     id: string,
@@ -24,9 +26,10 @@ export class ServerlessV1Microservices extends Construct {
     super(scope, id);
     this.productFn = this.createProductFunction(props);
     this.checkoutFn = this.createCheckOutFunction(props);
+    this.ordersFn = this.createOrdersFunction(props);
   }
 
-  getFnProps(): NodejsFunctionProps {
+  private getFnProps(): NodejsFunctionProps {
     const nodeJsFnProps: NodejsFunctionProps = {
       bundling: {
         externalModules: ['aws-sdk'],
@@ -37,32 +40,49 @@ export class ServerlessV1Microservices extends Construct {
     return nodeJsFnProps;
   }
 
-  createProductFunction(props: IServerlessV1MicroserviceProps): NodejsFunction {
+  private createProductFunction(
+    props: IServerlessV1MicroserviceProps
+  ): NodejsFunction {
     const productFn = new NodejsFunction(this, 'productsLambdaFn', {
       entry: path.join(__dirname, '/../../src/products/index.ts'),
-      ...this.getFnProps(),
       environment: {
         DYNAMODB_TABLE_NAME: props.productsTable.tableName,
         PRIMARY_KEY: 'id',
       },
+      ...this.getFnProps(),
     });
 
     props.productsTable.grantReadWriteData(productFn);
     return productFn;
   }
-  createCheckOutFunction(
+  private createCheckOutFunction(
     props: IServerlessV1MicroserviceProps
   ): NodejsFunction {
     const checkOutFn = new NodejsFunction(this, 'checkOutLambdaFn', {
       entry: path.join(__dirname, '/../../src/check-out/index.ts'),
-      ...this.getFnProps(),
       environment: {
         DYNAMODB_TABLE_NAME: props.checkOutTable.tableName,
         PRIMARY_KEY: 'username',
       },
+      ...this.getFnProps(),
     });
 
     props.checkOutTable.grantReadWriteData(checkOutFn);
     return checkOutFn;
+  }
+
+  private createOrdersFunction(
+    props: IServerlessV1MicroserviceProps
+  ): NodejsFunction {
+    const ordersFn = new NodejsFunction(this, 'ordersLambdaFn', {
+      entry: path.join(__dirname, '/../../src/orders/index.ts'),
+      environment: {
+        DYNAMODB_TABLE_NAME: props.ordersTable.tableName,
+        PRIMARY_KEY: 'id',
+      },
+      ...this.getFnProps(),
+    });
+    props.ordersTable.grantReadWriteData(ordersFn);
+    return ordersFn;
   }
 }
