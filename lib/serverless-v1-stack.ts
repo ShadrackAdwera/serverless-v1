c7d059c30db2cb63587ee78f8b43fe2c6e116cfd
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-21 11:37:07 +0300

    configure event bus in the root serverless stack

--- lib/serverless-v1-stack.ts
+++ lib/serverless-v1-stack.ts
@@ -4,6 +4,7 @@ import { Construct } from 'constructs';
 import { ServerlessV1Database } from './database/infrastructure';
 import { ServerlessV1Microservices } from './api/microservices';
 import { ServerlessV1ApiGateway } from './api/api-gateway';
+import { ServerlessV1EventBus } from './events/eventbus';
 
 // class open for extension but closed for modification
 export class ServerlessV1Stack extends Stack {
@@ -14,10 +15,17 @@ export class ServerlessV1Stack extends Stack {
     const microservice = new ServerlessV1Microservices(this, 'Microservice', {
       productsTable: database.productsTable,
       checkOutTable: database.checkOutTable,
+      ordersTable: database.ordersTable,
     });
     const apiGateway = new ServerlessV1ApiGateway(this, 'ApiGateway', {
       productFn: microservice.productFn,
       checkOutFn: microservice.checkoutFn,
+      ordersFn: microservice.ordersFn,
+    });
+
+    const eventBus = new ServerlessV1EventBus(this, 'EventBus', {
+      publisherFunction: microservice.checkoutFn,
+      targetFunction: microservice.ordersFn,
     });
   }
 }
