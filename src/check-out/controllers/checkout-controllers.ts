22d817d4adbbe82846d1c025dd247725a220d993
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-22 17:50:14 +0300

    process for publishing putEvents to EventBridge

--- src/check-out/controllers/checkout-controllers.ts
+++ src/check-out/controllers/checkout-controllers.ts
@@ -1,3 +1,4 @@
+import { APIGatewayEvent } from 'aws-lambda';
 import {
   ScanCommand,
   ScanCommandInput,
@@ -9,7 +10,69 @@ import {
   DeleteItemCommandInput,
 } from '@aws-sdk/client-dynamodb';
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import {
+  PutEventsCommand,
+  PutEventsCommandInput,
+  PutEventsCommandOutput,
+} from '@aws-sdk/client-eventbridge';
+
+import { ebClient } from '../libs/ebClient';
 import { ddbClient } from '../libs/ddbClient';
+
+interface ICheckOutPayload {
+  username: string;
+  totalPrice: number;
+  firstName?: string;
+  lastName?: string;
+  email: string;
+  address: string;
+  cardInfo: string;
+  paymentMethod: string;
+}
+
+const handleOrderPayload = (
+  checkOutRequest: ICheckOutPayload,
+  checkOutBasket: Record<string, any>
+): ICheckOutPayload => {
+  try {
+    let total = 0;
+    checkOutBasket.checkout.forEach(
+      (item: { price: number }) => (total += item.price)
+    );
+    checkOutRequest.totalPrice = total;
+    Object.assign(checkOutRequest, checkOutBasket);
+    return checkOutRequest;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const publishCheckOutEvent = async (
+  publishedEventPayload: ICheckOutPayload
+): Promise<PutEventsCommandOutput> => {
+  try {
+    const params: PutEventsCommandInput = {
+      Entries: [
+        {
+          Source: process.env.EVENT_SOURCE,
+          Detail: JSON.stringify(publishedEventPayload),
+          DetailType: process.env.EVENT_DETAILTYPE,
+          Resources: [],
+          EventBusName: process.env.EVENT_BUSNAME,
+        },
+      ],
+    };
+
+    const data = await ebClient.send(new PutEventsCommand(params));
+
+    console.log('Success, event sent; requestID:', data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
 const getCheckOutByUsername = async (username: string) => {
   const params: GetItemCommandInput = {
@@ -69,7 +132,42 @@ const createCheckoutBasket = async (body: string | null) => {
   }
 };
 
-const submitCheckouBasket = async () => {};
+const submitCheckoutBasket = async (event: APIGatewayEvent) => {
+  if (event.body === null) throw new Error('Provide an event body');
+  const payload: ICheckOutPayload = JSON.parse(event.body);
+  if (!payload.username) throw new Error('Provide a username');
+  let basketItems;
+
+  // 1. get basket items
+  try {
+    basketItems = await getCheckOutByUsername(payload.username);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+  if (!basketItems.checkout)
+    throw new Error('The user does not have any items in the basket');
+
+  // 2. prepare order payload
+  const orderPayload = handleOrderPayload(payload, basketItems.checkout);
+
+  // 3. publish event to event bridge
+
+  try {
+    await publishCheckOutEvent(orderPayload);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+
+  // 4. delete basket item
+  try {
+    await deleteCheckout(payload.username);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
 const deleteCheckout = async (username: string) => {
   const params: DeleteItemCommandInput = {
@@ -92,5 +190,5 @@ export {
   getCheckOut,
   deleteCheckout,
   createCheckoutBasket,
-  submitCheckouBasket,
+  submitCheckoutBasket,
 };
