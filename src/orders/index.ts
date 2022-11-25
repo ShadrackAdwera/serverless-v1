1562ac2b1eef375feddc53cc71604ff9842b6380
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-25 16:48:24 +0300

    set up lambda handler

--- src/orders/index.ts
+++ src/orders/index.ts
@@ -1,13 +1,50 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 
+import { getAllOrders, getOrders } from './controllers/orders-controllers';
+
 exports.handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
+  let apiResponse: APIGatewayProxyResult = {
+    statusCode: 404,
     body: JSON.stringify({
-      message: `Hellp from orders service path: ${event.path}, method: ${event.httpMethod}`,
+      message: `Invalid method / route`,
     }),
   };
+
+  try {
+    switch (event.httpMethod) {
+      case 'GET':
+        if (event.pathParameters && event.pathParameters.username) {
+          const orders = await getOrders(event.pathParameters.username);
+          apiResponse = {
+            statusCode: 200,
+            body: JSON.stringify({
+              orders,
+            }),
+          };
+        } else {
+          const orders = await getAllOrders();
+          apiResponse = {
+            statusCode: 200,
+            body: JSON.stringify({
+              orders,
+            }),
+          };
+        }
+    }
+    return apiResponse;
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message:
+          error instanceof Error
+            ? error.message
+            : 'An error occured, try again.',
+      }),
+    };
+  }
 };
