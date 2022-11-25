ad4c1a7193bcb91bd9dfdfc5af415b3d9227dce6
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-25 17:50:51 +0300

    subscribe to events from eb

--- src/orders/index.ts
+++ src/orders/index.ts
@@ -1,10 +1,40 @@
-import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import {
+  APIGatewayEvent,
+  APIGatewayProxyResult,
+  Context,
+  EventBridgeEvent,
+} from 'aws-lambda';
 
-import { getAllOrders, getOrders } from './controllers/orders-controllers';
+import {
+  getAllOrders,
+  getOrders,
+  createOrder,
+} from './controllers/orders-controllers';
+import { TOrder } from './libs/types';
 
 exports.handler = async (
-  event: APIGatewayEvent,
+  event: APIGatewayEvent | EventBridgeEvent<any, TOrder>,
   context: Context
+): Promise<APIGatewayProxyResult> => {
+  if ('detail-type' in event) {
+    await createOrder(event.detail);
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: 'Order placed!',
+      }),
+    };
+  } else {
+    return await apiGatewayInvocation(event);
+  }
+};
+
+const eventBridgeInvocation = async (event: EventBridgeEvent<any, TOrder>) => {
+  const basketItems = event.detail;
+};
+
+const apiGatewayInvocation = async (
+  event: APIGatewayEvent
 ): Promise<APIGatewayProxyResult> => {
   let apiResponse: APIGatewayProxyResult = {
     statusCode: 404,
@@ -12,7 +42,6 @@ exports.handler = async (
       message: `Invalid method / route`,
     }),
   };
-
   try {
     switch (event.httpMethod) {
       case 'GET':
