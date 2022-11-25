5160a2a05c653cc917af28641cfb1b027b574481
ShadrackAdwera <adweshshaddie@gmail.com>
2022-11-25 16:42:08 +0300

    create get order by username controller

--- src/orders/controllers/orders-controllers.ts
+++ src/orders/controllers/orders-controllers.ts
@@ -1,11 +1,31 @@
 import { GetItemCommand, GetItemCommandInput } from '@aws-sdk/client-dynamodb';
-import { marshall } from '@aws-sdk/util-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 
 import { ddbClient } from '../libs/ddbClient';
 
-const getOrders = () => {
+// const createOrder = async () => {
+
+// }
+
+const getOrders = async (username: string) => {
   const params: GetItemCommandInput = {
     TableName: process.env.DYNAMODB_TABLE_NAME,
-    Key: marshall({ username: '' }),
+    Key: marshall({ username }),
   };
+
+  try {
+    const { Item } = await ddbClient.send(new GetItemCommand(params));
+    return Item
+      ? {
+          orders: unmarshall(Item),
+        }
+      : {
+          message: 'No orders found',
+        };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
+
+export { getOrders };
